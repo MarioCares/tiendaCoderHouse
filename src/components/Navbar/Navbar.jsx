7c44ee078553f364121afdcbc5c46d8ed07664b0
Mario Cares <mario.cares.c@outlook.com>
2023-06-13 19:13:29 -0400

    fix burger

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,51 +1,56 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
-import { Container, Navbar } from "react-bulma-components";
+import { Container, Form, Icon, Navbar } from "react-bulma-components";
 import DropdownItem from "./DropdownItem.jsx";
 
-const NavbarComponent = ({ menu }) => (
-  <Navbar>
-    <Container>
-      <Navbar.Brand>
-        <Navbar.Item
-          renderAs="a"
-          href="#"
-          className="is-size-5 has-text-weight-semibold"
-        >
-          <img src={menu.brand.img} alt={menu.brand.name} /> {menu.brand.name}
-        </Navbar.Item>
-        <Navbar.Burger />
-      </Navbar.Brand>
-      <Navbar.Menu>
-        <Navbar.Container align="right" textSize={5} textWeight={"bold"}>
-          <Navbar.Item>
-            <div className="control has-icons-left">
-              <input className="input is-rounded" placeholder="Búsqueda" />
-              <span className="icon is-left">
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-              </span>
-            </div>
+const NavbarComponent = ({ menu }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <Navbar active={isActive}>
+      <Container>
+        <Navbar.Brand onClick={() => setIsActive(!isActive)}>
+          <Navbar.Item
+            renderAs="a"
+            href="#"
+            className="is-size-5 has-text-weight-semibold"
+          >
+            <img src={menu.brand.img} alt={menu.brand.name} /> {menu.brand.name}
           </Navbar.Item>
-          {menu.items &&
-            menu.items.map((item, index) =>
-              item.subitems ? (
-                <DropdownItem
-                  key={`${item}-${index}`}
-                  name={item.name}
-                  subItems={item.subitems}
-                />
-              ) : (
-                <Navbar.Item key={item.name} href={item.url}>
-                  {item.name}
-                </Navbar.Item>
-              )
-            )}
-        </Navbar.Container>
-      </Navbar.Menu>
-    </Container>
-  </Navbar>
-);
+          <Navbar.Burger />
+        </Navbar.Brand>
+        <Navbar.Menu>
+          <Navbar.Container align="right" textSize={5} textWeight={"bold"}>
+            <Navbar.Item>
+              <Form.Control>
+                <Form.Input placeholder="Búsqueda" className="is-rounded" />
+                <Icon align="left">
+                  <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </Icon>
+              </Form.Control>
+            </Navbar.Item>
+            {menu.items &&
+              menu.items.map((item, index) =>
+                item.subitems ? (
+                  <DropdownItem
+                    key={`${item}-${index}`}
+                    name={item.name}
+                    subItems={item.subitems}
+                  />
+                ) : (
+                  <Navbar.Item key={item.name} href={item.url}>
+                    {item.name}
+                  </Navbar.Item>
+                )
+              )}
+          </Navbar.Container>
+        </Navbar.Menu>
+      </Container>
+    </Navbar>
+  );
+};
 
 NavbarComponent.propTypes = {
   menu: PropTypes.shape({
